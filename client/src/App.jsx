d6fdd884b8c766edfee1e5bf4ee027c22decb5e4
Samuel Jacobs <97236680+samueljacobs98@users.noreply.github.com>
2023-12-06 18:58:56 +0000

    Create basic components (#2)
    * Created, organised and styled  basic components
    * Updated port in postman environment variables
    * install and import lodash

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,49 @@
 import "./App.scss";
+import Button from "./components/Button/Button";
+import ButtonContainer from "./components/ButtonContainer/ButtonContainer";
+import FloorDisplay from "./components/FloorDisplay/FloorDisplay";
+import Layout from "./components/Layout/Layout";
+import LiftStatus from "./components/LiftStatus/LiftStatus";
+import StatusTracker from "./components/StatusTracker/StatusTracker";
+import union from "lodash/union";
 
 function App() {
-  return <div>Hello, World!</div>;
+  const liftConfig = {
+    lifts: {
+      0: {
+        servicedFloors: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      },
+      1: {
+        servicedFloors: [0, 7, 8, 9, 10],
+      },
+    },
+  };
+  const floors = union(
+    liftConfig.lifts[0].servicedFloors,
+    liftConfig.lifts[1].servicedFloors
+  );
+
+  const userFloor = 0;
+
+  const onClick = (e) => {
+    console.log(e.target.value);
+  };
+
+  const buttons = floors.map((floor) => {
+    return <Button key={floor} onClick={onClick} floorNumber={floor} />;
+  });
+
+  return (
+    <Layout>
+      <FloorDisplay floor={userFloor} />
+      <ButtonContainer>{buttons}</ButtonContainer>
+      <StatusTracker>
+        <LiftStatus liftNumber={1} queue={[1, 3, 4]} />
+        <LiftStatus liftNumber={2} queue={[1, 5, 9]} />
+        <LiftStatus liftNumber={5} queue={[2, 6, 7]} />
+      </StatusTracker>
+    </Layout>
+  );
 }
 
 export default App;
---
+++ client/src/components/StatusTracker/StatusTracker.jsx
@@ -0,0 +1,7 @@
+import "./StatusTracker.scss";
+
+const StatusTracker = ({ children }) => {
+  return <div className="status-tracker">{children}</div>;
+};
+
+export default StatusTracker;
