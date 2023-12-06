2f134362ed9854d2ec73bdeddf97b22fc94d147a
Samuel Jacobs <sammyjacobscontact@gmail.com>
2023-12-06 18:48:59 +0000

    install and import lodash

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,25 @@ import FloorDisplay from "./components/FloorDisplay/FloorDisplay";
 import Layout from "./components/Layout/Layout";
 import LiftStatus from "./components/LiftStatus/LiftStatus";
 import StatusTracker from "./components/StatusTracker/StatusTracker";
+import union from "lodash/union";
 
 function App() {
+  const liftConfig = {
+    lifts: {
+      0: {
+        servicedFloors: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      },
+      1: {
+        servicedFloors: [0, 7, 8, 9, 10],
+      },
+    },
+  };
+  const floors = union(
+    liftConfig.lifts[0].servicedFloors,
+    liftConfig.lifts[1].servicedFloors
+  );
+
   const userFloor = 0;
-  const floors = new Array(15).fill(0).map((_, i) => i);
 
   const onClick = (e) => {
     console.log(e.target.value);
