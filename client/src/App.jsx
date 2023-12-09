b66bfc589c4da2bc5b9a4143b768af045ef5e531
Samuel Jacobs <sammyjacobscontact@gmail.com>
2023-12-09 12:02:43 +0000

    refactored DestinationTracker, App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,14 @@ import ButtonContainer from "./components/ButtonContainer/ButtonContainer";
 import FloorDisplay from "./components/FloorDisplay/FloorDisplay";
 import Layout from "./components/Layout/Layout";
 import { getData, postData } from "./utils/api";
-import { useEffect, useState } from "react";
-import union from "lodash/union";
 import Modal from "./components/Modal/Modal";
-import DestinationTracker from "./components/DestinationTracker/DestinationTracker";
+import DestinationTrackerContainer from "./components/DestinationTrackerContainer/DestinationTrackerContainer";
+import DestinationTrackerItem from "./components/DestinationTrackerItem/DestinationTrackerItem";
+import { useEffect, useRef, useState } from "react";
+import union from "lodash/union";
 
 function App() {
+  // TODO: context API?
   const [liftConfig, setLiftConfig] = useState(null);
   const [liftStatus, setLiftStatus] = useState(null);
   const [floors, setFloors] = useState([]);
@@ -20,22 +22,25 @@ function App() {
     at: null,
   });
   const [queueData, setQueueData] = useState([]);
+  const [arrived, setArrived] = useState(new Set());
 
-  const userFloor = process.env.REACT_APP_FLOOR;
-  if (!userFloor) {
+  const userFloorRef = useRef(parseInt(process.env.REACT_APP_FLOOR));
+  if (userFloorRef.current == null) {
     throw new Error("REACT_APP_FLOOR env variable not set");
   }
+  const pollingIntervalRef = useRef(
+    process.env.REACT_APP_POLLING_INTERVAL || 10
+  );
 
   useEffect(() => {
     getData("lift/config", (data) => setLiftConfig(data));
     getData("lift/status", (data) => setLiftStatus(data));
 
     // TODO: is this polling approach the best way to do this?
-    // TODO: use env variable for interval
-    // const interval = setInterval(() => {
-    //   getData("lift/status", ({ lifts }) => setLiftStatus(lifts));
-    // }, 1000);
-    // return () => clearInterval(interval);
+    const interval = setInterval(() => {
+      getData("lift/status", (data) => setLiftStatus(data));
+    }, pollingIntervalRef.current * 1000);
+    return () => clearInterval(interval);
   }, []);
 
   useEffect(() => {
@@ -50,18 +55,22 @@ function App() {
   useEffect(() => {
     if (!liftStatus) return;
     setQueueData(
+      // can this be done more efficiently?
       Object.entries(liftStatus.lifts).reduce(
-        (acc, [lift, { destinations }]) => {
+        (acc, [lift, { floor, destinations }]) => {
+          if (floor === userFloorRef.current) {
+            setArrived((arrived) => new Set(arrived).add(lift));
+          }
+
           destinations.forEach((destination) => {
-            const floorObj = acc.find(
-              (floorObj) => floorObj.floor === destination
-            );
+            const floorObj = acc.find(({ floor }) => floor === destination);
             if (floorObj) {
               floorObj.lifts.push(lift);
             } else {
               acc.push({ floor: destination, lifts: [lift] });
             }
           });
+
           return acc;
         },
         []
@@ -70,18 +79,17 @@ function App() {
   }, [liftStatus]);
 
   const onClick = async (e) => {
-    const to_floor = parseInt(e.target.value);
+    const toFloor = parseInt(e.target.value);
 
     if (liftStatus.lifts) {
-      Object.entries(liftStatus.lifts).forEach((i) => console.log(i));
       const liftAlreadyGoingToFloor = Object.entries(liftStatus.lifts).find(
-        ([_, { floor, destinations }]) => destinations.includes(to_floor)
+        ([_, { destinations }]) => destinations.includes(toFloor)
       );
 
       if (liftAlreadyGoingToFloor) {
         const lift = liftAlreadyGoingToFloor[0];
         const floor = liftAlreadyGoingToFloor[1].floor;
-        setModalData({ lift, to: to_floor, at: floor });
+        setModalData({ lift, to: toFloor, at: floor });
         setShowModal(true);
         return;
       }
@@ -89,19 +97,19 @@ function App() {
 
     const res = await postData(
       "lift/request",
-      { from_floor: userFloor, to_floor },
+      { from_floor: userFloorRef.current, to_floor: toFloor },
       (data) => data
     );
 
     await getData("lift/status", (data) => setLiftStatus(data));
 
-    setModalData({ lift: res.lift, to: to_floor });
+    setModalData({ lift: res.lift, to: toFloor });
     setShowModal(true);
   };
 
   return (
     <Layout isLoading={!Boolean(liftConfig && liftStatus)}>
-      <FloorDisplay floor={userFloor} />
+      <FloorDisplay floor={userFloorRef.current} />
       {/* TODO: Add loading element(s) */}
       {liftConfig && liftStatus && (
         <>
@@ -112,7 +120,18 @@ function App() {
               );
             })}
           </ButtonContainer>
-          <DestinationTracker queueData={queueData} />
+          <DestinationTrackerContainer>
+            {queueData.map((data) => {
+              return (
+                <DestinationTrackerItem
+                  key={data.floor}
+                  floor={data.floor}
+                  lifts={data.lifts}
+                  arrived={arrived}
+                />
+              );
+            })}
+          </DestinationTrackerContainer>
         </>
       )}
       {showModal && (
